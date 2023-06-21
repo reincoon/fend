beaf7b01d4f082669ce8f5233ddc2a742e155c5e
reincoon <irinawalch@gmail.com>
2023-06-21 21:35:43 +0100

    second project

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,47 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// Configure express to use body-parser as middle-ware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors for cross-origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// GET route that returns the projectData object
+app.get('/all', getProjectData);
+
+function getProjectData(req, res){
+  res.send(projectData);
+};
+
+// POST route that adds incoming data to projectData
+app.post('/addData', addData);
+function addData(req, res) {
+    let newData = {
+      name: req.body.name,
+      temp: req.body.temp,
+      date: req.body.date,
+      content: req.body.content
+    }
+    // projectData.push(newData);
+    projectData = newData;
+    res.send(projectData);
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,101 @@
 /* Global Variables */
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'a017c0f46e284252c34f5195db496856&units=imperial';
+
+/===================================================/
+
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const d = new Date();
+const newDate = d.getMonth() + 1 +'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.querySelector('#generate').addEventListener('click', generateWeather);
+
+
+/** Function to generate URL*/
+function buildURL(){
+    
+  let zipCode = document.querySelector('#zip').value;
+  let zip = '?zip='+zipCode+',us&appid=';
+  return baseURL+zip+apiKey;
+  
+}
+/* Function called by event listener */
+async function generateWeather(e) {
+  const generatedURL = buildURL();
+  let feelings = document.querySelector('#feelings').value;
+  await getWeatherData(generatedURL)
+  .then(function(data){
+    const postData = {
+      name: data.name, 
+      temp: data.main.temp, 
+      date: newDate, 
+      content: feelings
+    };
+    postWeatherData('/addData', postData)
+  })
+  .then(function(){
+    retrieveData();
+  })
+}
+
+/* Function to GET weather data from API */
+const getWeatherData = async(url) => {
+  const response = await fetch(url);
+
+  try {
+    const data = await response.json();
+    return data;
+  } catch(error) {
+    console.log('Error:', error);
+  }
+
+}
+
+/* Function to POST weather data */
+const postWeatherData = async(url, data = {}) => {
+  const response = await fetch(`http://localhost:8000${url}`, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  try {
+    const newData = response.json();
+    console.log(newData);
+    return newData;
+  } catch(error) {
+    console.log('Error:', error);
+  }
+
+}
+
+/* Function to update the UI dynamically */
+const retrieveData = async() => {
+  //const req = await fetch('/all');
+  const request = await fetch('http://localhost:8000/all');
+  try{
+    const allData = await request.json();
+    console.log(allData);
+    //const latestEntry = allData[allData.length - 1];
+    document.getElementById("date").innerHTML = `Date: ${allData.date}`;
+    document.getElementById("temp").innerHTML = `Temperature: ${allData.temp}°C`;
+    document.getElementById("content").innerHTML = `Feeling: ${allData.content}`;
+  } catch(error) {
+    console.log('Error:', error);
+  }
+}
+
+/* Function to GET project data */
+const getProjectData = async (url) => {
+  const response = await fetch(`http://localhost:8000${url}`)
+  try{
+      const data = await response.json();
+      return data
+  }catch(error){
+      console.log('error ', error)
+  }
+}
